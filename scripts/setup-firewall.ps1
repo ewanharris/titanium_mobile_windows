c7bffe89bb91cc2041ef82f2f5f7dc81d039d581
Ewan Harris <ewanharris93@gmail.com>
2018-04-13 17:24:22 +0100

    Add TCP rule for a laugh

--- scripts/setup-firewall.ps1
+++ scripts/setup-firewall.ps1
@@ -16,18 +16,40 @@ $nodeExe = $nodeInfo.Source
 Write-Host "Node exe is " $nodeExe
 Write-Host "Node version is " $nodeVersion
 
-$ruleName = "Node $($nodeVersion) UDP"
+# Setup UDP
 
-$existingRule = Get-NetFirewallRule -DisplayName $ruleName -ErrorAction SilentlyContinue
+$ruleNameUDP= "Node $($nodeVersion) UDP"
 
-if ($existingRule.length -ne 0)
+$existingRuleUDP = Get-NetFirewallRule -DisplayName $ruleNameUDP -ErrorAction SilentlyContinue
+
+if ($existingRuleUDP.length -ne 0)
 {
-    Write-Host "Rule exists, removing"
-    Remove-NetFirewallRule -DisplayName $ruleName
+    Write-Host "UDP rule exists, removing"
+    Remove-NetFirewallRule -DisplayName $ruleNameUDP
 }
 
-Write-Host "Running New-NetFirewallRule with args -DisplayName $($ruleName) -Direction Inbound  -Protocol UDP -Action Allow -EdgeTraversalPolicy Allow -Program $($nodeExe)"
-New-NetFirewallRule -DisplayName $ruleName -Direction Inbound  -Protocol UDP -Action Allow -EdgeTraversalPolicy Allow -Program $nodeExe
- 
-$ruleInfo = Get-NetFirewallRule -DisplayName $ruleName | Get-NetFirewallApplicationFilter
-Write-Host $ruleInfo.AppPath
+
+Write-Host "Running New-NetFirewallRule with args -DisplayName $($ruleNameUDP) -Direction Inbound  -Protocol UDP -Action Allow -EdgeTraversalPolicy Allow -Program $($nodeExe)"
+New-NetFirewallRule -DisplayName $ruleNameUDP -Direction Inbound  -Protocol UDP -Action Allow -EdgeTraversalPolicy Allow -Program $nodeExe
+
+$ruleInfo = Get-NetFirewallRule -DisplayName $ruleNameUDP | Get-NetFirewallApplicationFilter
+Write-Host $ruleInfo.Program
+Write-Host
+
+# Setup TCP
+
+$ruleNameTCP = "Node $($nodeVersion) TCP"
+
+$existingRuleTCP = Get-NetFirewallRule -DisplayName $ruleNameTCP -ErrorAction SilentlyContinue
+
+if ($existingRuleTCP.length -ne 0)
+{
+    Write-Host "TCP rule exists, removing"
+    Remove-NetFirewallRule -DisplayName $ruleNameTCP
+}
+
+Write-Host "Running New-NetFirewallRule with args -DisplayName $($ruleNameTCP) -Direction Inbound  -Protocol TCP -Action Allow -EdgeTraversalPolicy Allow -Program $($nodeExe)"
+New-NetFirewallRule -DisplayName $ruleNameTCP -Direction Inbound  -Protocol TCP -Action Allow -EdgeTraversalPolicy Allow -Program $nodeExe
+
+$ruleInfo = Get-NetFirewallRule -DisplayName $ruleNameTCP | Get-NetFirewallApplicationFilter
+Write-Host $ruleInfo.Program
